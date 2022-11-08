3fcfea3a884e21ee73dfd0a9bf89e6fd0f730a3a
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-08 22:48:01 +0600

    mongodb setup and dbUser created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +10,19 @@ const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.47nvmfs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('ankanDb').collection('services');
+    }
+    finally{
+
+    }
+}
 
 app.get('/', (req, res) =>{
     res.send('ankan print server is running');
